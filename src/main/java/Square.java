b15abacc1d83bfabd6efff9ff57581a6c91e372b
a.hosseini <a.hosseini@inside.sahab.ir>
2021-11-01 21:02:03 +0330

    Fix square area tests

---
+++ src/main/java/Square.java
@@ -0,0 +1,13 @@
+public class Square {
+
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    public int computeArea() {
+        return side * side;
+    }
+
+}
