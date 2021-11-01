8ba00e8df705f15823be477c31aabcb33fb6d429
a.hosseini <a.hosseini@inside.sahab.ir>
2021-11-01 21:03:53 +0330

    Refactor square and Rectangle classes

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,4 +1,4 @@
-public class Rectangle {
+public class Rectangle implements Shape {
 
     private int height;
     private int width;
--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,4 +1,4 @@
-public class Square {
+public class Square implements Shape {
 
     private int side;
 
