88919439684479e9080ff8c8d96e8cb12b42de29
a.hosseini <a.hosseini@inside.sahab.ir>
2021-11-01 20:31:40 +0330

    write first tests

--- src/test/java/RectangleAreaTest.java
+++ src/test/java/RectangleAreaTest.java
@@ -1,18 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class RectangleAreaTest {
 
+    private Rectangle rectangle;
+    private int result;
+
     @Given("^height and width of rectangle, (\\d+) and (\\d+)$")
-    public void heightAndWidthOfRectangleAnd(int arg0, int arg1) {
+    public void heightAndWidthOfRectangleAnd(int height, int width) {
+        rectangle = new Rectangle(height, width);
     }
 
     @When("^I calculate area of rectangle$")
     public void iCalculateAreaOfRectangle() {
+        result = rectangle.computeArea();
     }
 
     @Then("^I expect the result (\\d+)$")
-    public void iExpectTheResult(int arg0) {
+    public void iExpectTheResult(int expectedResult) {
+        assertEquals(expectedResult, result);
     }
 }
