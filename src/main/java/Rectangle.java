9bcd4ad8a97c2290bdde3761bfac038583a63725
a.hosseini <a.hosseini@inside.sahab.ir>
2021-11-01 20:33:25 +0330

    Fix tests for compile

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+
+    private int height;
+    private int width;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int computeArea() {
+        return 0;
+    }
+}
